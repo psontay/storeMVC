dc51a1990aa7903bdc6e7d7cb70f29ba04ee6731
sontaypham <phamsontay2206@gmail.com>
2025-10-23 07:21:36 +0700

    initial

---
+++ src/main/java/com/sontaypham/storemvc/StoreMvcApplication.java
@@ -0,0 +1,13 @@
+package com.sontaypham.storemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sontaypham/storemvc/StoreMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sontaypham.storemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
