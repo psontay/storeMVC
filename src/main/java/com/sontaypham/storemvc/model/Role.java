d87ec82f4ed12f3eae774c6e5bad6fd1334b39af
sontaypham <phamsontay2206@gmail.com>
2025-10-25 13:47:46 +0700

    feat(Role)

--- src/main/java/com/sontaypham/storemvc/model/Role.java
+++ src/main/java/com/sontaypham/storemvc/model/Role.java
@@ -1,11 +1,10 @@
 package com.sontaypham.storemvc.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
 
 @Entity
 @Table( name = "roles")
@@ -18,7 +17,12 @@ import lombok.experimental.FieldDefaults;
 public class Role {
     @Id
     @Column(name = "name")
+    @EqualsAndHashCode.Exclude
     String name;
     @Column(name = "description")
     String description;
+
+    @ManyToMany(fetch = FetchType.EAGER , cascade = CascadeType.MERGE)
+    @JoinTable( name = "roles_permissions" , joinColumns = @JoinColumn(name = "roles_name") , inverseJoinColumns = @JoinColumn(name = "permissions_name"))
+    Set<Permission> permissions;
 }
