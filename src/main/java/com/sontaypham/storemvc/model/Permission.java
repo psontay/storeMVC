47f1893dcb8698d1be63fc8c016a314bacecf9b7
sontaypham <phamsontay2206@gmail.com>
2025-10-25 13:43:23 +0700

    feat(Permission)

--- src/main/java/com/sontaypham/storemvc/model/Permission.java
+++ src/main/java/com/sontaypham/storemvc/model/Permission.java
@@ -1,9 +1,23 @@
 package com.sontaypham.storemvc.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Table(name = "permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Permission {
+    @Id
+    @EqualsAndHashCode.Exclude
+    String name;
+    String description;
 }
