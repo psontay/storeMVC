bcec54ec860f50b49b62efe61b5d2161b2a56d20
sontaypham <phamsontay2206@gmail.com>
2025-10-25 21:35:27 +0700

    feat(CustomAuthenticationSuccessHandler)

---
+++ src/main/java/com/sontaypham/storemvc/configuration/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,30 @@
+package com.sontaypham.storemvc.configuration;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        var authorities = authentication.getAuthorities();
+        log.info("login success user : " + authentication.getName());
+        log.info("with role : " + authorities);
+        String redirectUrl = "/";
+        if (authorities.stream().anyMatch(a -> a.getAuthority().contains("ADMIN"))) {
+            redirectUrl = "/admin/dashboard";
+        } else if (authorities.stream().anyMatch(a -> a.getAuthority().contains("USER"))) {
+            redirectUrl = "/";
+        }
+        response.sendRedirect(redirectUrl);
+    }
+}
--- src/main/java/com/sontaypham/storemvc/configuration/SecurityConfig.java
+++ src/main/java/com/sontaypham/storemvc/configuration/SecurityConfig.java
@@ -1,8 +1,27 @@
 package com.sontaypham.storemvc.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    private final String[] publicEndpoints = {
+      "/" ,
+      "/auth/login",
+            "/auth/register",
+            "/auth/logout",
+    };
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(HttpSecurity http , AuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
 
+    }
 }
