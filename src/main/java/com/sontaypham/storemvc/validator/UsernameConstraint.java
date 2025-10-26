402e837dec7a5f9a5644bafbc509d578ad82af9e
sontaypham <phamsontay2206@gmail.com>
2025-10-26 16:53:48 +0700

    feat(Username-validation) : create validation for username

---
+++ src/main/java/com/sontaypham/storemvc/validator/UsernameConstraint.java
@@ -0,0 +1,19 @@
+package com.sontaypham.storemvc.validator;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UsernameValidator.class)
+public @interface UsernameConstraint {
+    String message() default "USERNAME_TYPE_INVALID";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/sontaypham/storemvc/validator/UsernameValidator.java
@@ -0,0 +1,17 @@
+package com.sontaypham.storemvc.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UsernameValidator implements ConstraintValidator<UsernameConstraint , String> {
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
+        if ( username == null || username.isBlank()) return true;
+        return username.chars().anyMatch(Character::isUpperCase) && username.chars().anyMatch( Character::isDigit ) && username.chars().anyMatch( c -> !Character.isLetterOrDigit(c) && !Character.isWhitespace(c));
+    }
+
+    @Override
+    public void initialize(UsernameConstraint usernameConstraint) {
+        ConstraintValidator.super.initialize(usernameConstraint);
+    }
+}
