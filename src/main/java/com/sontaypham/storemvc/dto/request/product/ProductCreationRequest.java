76436419ec72a4cee272c8328fbf33fc3be87477
sontaypham <phamsontay2206@gmail.com>
2025-11-10 22:19:25 +0700

    feat(Product-Creation-Request) : update product creation request to add field

--- src/main/java/com/sontaypham/storemvc/dto/request/product/ProductCreationRequest.java
+++ src/main/java/com/sontaypham/storemvc/dto/request/product/ProductCreationRequest.java
@@ -1,3 +1,30 @@
 package com.sontaypham.storemvc.dto.request.product;
 
-public class ProductCreationRequest {}
+import com.sontaypham.storemvc.enums.ProductStatus;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductCreationRequest {
+    String name;
+    String description;
+    int age;
+    String origin;
+    String imageUrl;
+    int stockQuantity;
+    BigDecimal price;
+    BigDecimal originalPrice;
+    BigDecimal discountedPrice;
+    int discountPercent;
+    ProductStatus status;
+    UUID categoryId;
+    UUID supplierId;
+}
