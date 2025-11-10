f33ac600599514cc084092806629239aafa6ecda
sontaypham <phamsontay2206@gmail.com>
2025-11-10 22:34:24 +0700

    feat(Supplier-Repository) : update method

--- src/main/java/com/sontaypham/storemvc/repository/SupplierRepository.java
+++ src/main/java/com/sontaypham/storemvc/repository/SupplierRepository.java
@@ -1,9 +1,20 @@
 package com.sontaypham.storemvc.repository;
 
 import com.sontaypham.storemvc.model.Supplier;
+
+import java.util.Optional;
 import java.util.UUID;
+
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SupplierRepository extends JpaRepository<Supplier, UUID> {}
+public interface SupplierRepository extends JpaRepository<Supplier, UUID> {
+    @Nonnull
+    Optional<Supplier> findById(@Nonnull UUID id);
+    @Nonnull
+    Optional<Supplier> findByName(@Nonnull String name);
+    Optional<Supplier> findByEmail(@Nonnull String email);
+    Optional<Supplier> findByTelPhone(@Nonnull String telPhone);
+}
