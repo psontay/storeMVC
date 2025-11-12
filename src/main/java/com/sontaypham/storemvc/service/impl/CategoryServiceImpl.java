55c0f2dfd4ae3749533f2305f39fd1a6c3b150d6
sontaypham <phamsontay2206@gmail.com>
2025-11-12 08:51:27 +0700

    feat(Category-Service-Impl): create impl service for category

---
+++ src/main/java/com/sontaypham/storemvc/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sontaypham.storemvc.service.impl;
+
+import com.sontaypham.storemvc.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+}
