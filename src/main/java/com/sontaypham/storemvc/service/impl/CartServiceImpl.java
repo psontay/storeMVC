0287d790fc15ae802e34671bf87e7f2394a2810e
sontaypham <phamsontay2206@gmail.com>
2025-11-12 08:48:47 +0700

    perf(Cart-Service-Impl)

--- src/main/java/com/sontaypham/storemvc/service/impl/CartServiceImpl.java
+++ src/main/java/com/sontaypham/storemvc/service/impl/CartServiceImpl.java
@@ -9,7 +9,6 @@ import com.sontaypham.storemvc.repository.CartRepository;
 import com.sontaypham.storemvc.repository.ProductRepository;
 import com.sontaypham.storemvc.repository.UserRepository;
 import com.sontaypham.storemvc.service.CartService;
-import com.sontaypham.storemvc.util.SecurityUtilBean;
 import com.sontaypham.storemvc.util.SecurityUtilStatic;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
@@ -17,9 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 
@@ -34,7 +31,6 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public CartResponse getCartByUserId() {
-        User user = getCurrentUser();
         Cart cart = getCurrentUserCart();
         cart.recalcTotal();
         return cartMapper.fromEntityToResponse(cart);
@@ -44,7 +40,7 @@ public class CartServiceImpl implements CartService {
     @Transactional
     public void addToCart(UUID productId, int quantity) {
         User user = getCurrentUser();
-        Cart cart = getCurrentUserCart();
+        Cart cart = getOrCreateCart(user);
         Product product = productRepository.findById(productId).orElseThrow(() -> new ApiException(ErrorCode.PRODUCT_NOT_FOUND));
         Optional<CartItem> existingItem = cart.getItems().stream().filter(i -> i.getProduct().getId().equals(product.getId())).findFirst();
         if ( existingItem.isPresent()) {
@@ -52,15 +48,17 @@ public class CartServiceImpl implements CartService {
             item.setQuantity(item.getQuantity() + quantity);
             item.recalcSubtotal();
         }else {
-            cart.getItems().add(CartItem.builder().cart(cart).product(product).quantity(quantity).unitPrice(product.getPrice()).build());
+            CartItem newItem = CartItem.builder().cart(cart).product(product).quantity(quantity).unitPrice(product.getPrice()).build();
+            newItem.recalcSubtotal();
+            cart.getItems().add(newItem);
         }
         cart.recalcTotal();
         cartRepository.save(cart);
     }
 
     @Override
+    @Transactional
     public void updateQuantity( UUID productId, int quantity) {
-        User user = getCurrentUser();
         Cart cart = getCurrentUserCart();
         Product product = productRepository.findById(productId).orElseThrow(() -> new ApiException(ErrorCode.PRODUCT_NOT_FOUND));
         Optional<CartItem> existingItem = cart.getItems().stream().filter(i -> i.getProduct().getId().equals(product.getId())).findFirst();
@@ -69,7 +67,6 @@ public class CartServiceImpl implements CartService {
             item.setQuantity(quantity);
             item.recalcSubtotal();
             cart.recalcTotal();
-            cartRepository.save(cart);
         }else {
             throw new ApiException(ErrorCode.PRODUCT_NOT_FOUND_IN_CART);
         }
@@ -78,15 +75,18 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
+    @Transactional
     public void removeFromCart( UUID productId) {
         Cart cart = getCurrentUserCart();
         Product product = productRepository.findById(productId).orElseThrow(() -> new ApiException(ErrorCode.PRODUCT_NOT_FOUND));
         CartItem itemRemove = cart.getItems().stream().filter( i -> i.getProduct().getId().equals(product.getId())).findFirst().orElseThrow(() -> new ApiException(ErrorCode.PRODUCT_NOT_FOUND));
         cart.removeItem(itemRemove);
+        cart.recalcTotal();
         cartRepository.save(cart);
     }
 
     @Override
+    @Transactional
     public void clearCart() {
         Cart cart = getCurrentUserCart();
         cart.getItems().clear();
@@ -98,7 +98,13 @@ public class CartServiceImpl implements CartService {
     private User getCurrentUser() {
         return userRepository.findById(SecurityUtilStatic.getUserId()).orElseThrow(() -> new ApiException(ErrorCode.UNAUTHENTICATED));
     }
+    private Cart getOrCreateCart( User user) {
+        return cartRepository.findByUserId(user.getId()).orElseGet(() -> cartRepository.save(Cart.builder().user(getCurrentUser()).build()));
+    }
     private Cart getCurrentUserCart() {
-        return cartRepository.findByUserId(SecurityUtilStatic.getUserId()).orElseGet(() -> cartRepository.save(Cart.builder().user(getCurrentUser()).build()));
+        return cartRepository.findByUserId(SecurityUtilStatic.getUserId())
+                             .orElseGet(() -> cartRepository.save(
+                                     Cart.builder().user(getCurrentUser()).build()
+                                                                 ));
     }
 }
