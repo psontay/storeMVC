e401ca42c673fe3ce8cda0a55b4995bfe987b8d5
sontaypham <phamsontay2206@gmail.com>
2025-11-12 12:14:53 +0700

    feat(Category-Mapper): create mapper for category

---
+++ src/main/java/com/sontaypham/storemvc/mapper/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.sontaypham.storemvc.mapper;
+
+import com.sontaypham.storemvc.dto.request.category.CategoryCreationRequest;
+import com.sontaypham.storemvc.dto.request.category.CategoryUpdateRequest;
+import com.sontaypham.storemvc.dto.response.category.CategoryResponse;
+import com.sontaypham.storemvc.model.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper( componentModel = "spring")
+public interface CategoryMapper {
+    public Category fromCreationToEntity(CategoryCreationRequest request);
+
+    void updateEntityFromRequest(CategoryUpdateRequest request, @MappingTarget Category category);
+
+    public CategoryResponse fromEntityToResponse(Category entity);
+}
