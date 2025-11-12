f63656e3c420aee838f7f401cc4e61d42c07feef
sontaypham <phamsontay2206@gmail.com>
2025-11-12 12:21:56 +0700

    feat(Category-Repository) : create repository for category

--- src/main/java/com/sontaypham/storemvc/repository/CategoryRepository.java
+++ src/main/java/com/sontaypham/storemvc/repository/CategoryRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import jakarta.annotation.Nonnull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +17,9 @@ public interface CategoryRepository extends JpaRepository<Category, UUID> {
     Optional<Category> findById(@Nonnull UUID id);
     @Nonnull
     Optional<Category> findByName(@Nonnull String name);
+    @Nonnull
+    Optional<Category> findByNameIgnoreCase(String name);
+    boolean existsByName(String name);
+    Page<Category> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
 }
