82106daf5555320887ee0b08566daa48d679c6ad
sontaypham <phamsontay2206@gmail.com>
2025-11-15 10:38:10 +0700

    feat(Order-Update) : create order update req

---
+++ src/main/java/com/sontaypham/storemvc/dto/request/order/OrderUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.sontaypham.storemvc.dto.request.order;
+
+import com.sontaypham.storemvc.enums.OrderStatus;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OrderUpdateRequest {
+    String shippingAddress;
+    OrderStatus orderStatus;
+}
