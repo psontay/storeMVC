e816779a900866069e2431e7e009cfd6ccb0d695
sontaypham <phamsontay2206@gmail.com>
2025-11-16 09:19:51 +0700

    feat(Supplier-Update-Request): create supp update request dto

---
+++ src/main/java/com/sontaypham/storemvc/dto/request/supplier/SupplierUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.sontaypham.storemvc.dto.request.supplier;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SupplierUpdateRequest {
+    String name;
+    String fullName;
+    String email;
+    String telPhone;
+}
