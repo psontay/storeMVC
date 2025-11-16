4a1812b28c2450105ac01df09ebf90eb4dc29998
sontaypham <phamsontay2206@gmail.com>
2025-11-16 09:18:29 +0700

    feat(Supplier-Creation-Request): create field

--- src/main/java/com/sontaypham/storemvc/dto/request/supplier/SupplierCreationRequest.java
+++ src/main/java/com/sontaypham/storemvc/dto/request/supplier/SupplierCreationRequest.java
@@ -1,3 +1,17 @@
 package com.sontaypham.storemvc.dto.request.supplier;
 
-public class SupplierCreationRequest {}
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SupplierCreationRequest {
+    String name;
+    String fullName;
+    String email;
+    String telPhone;
+}
