79c18baa05032affb2aa7c6b318b7ea992bcaa62
sontaypham <phamsontay2206@gmail.com>
2025-12-14 11:07:54 +0700

    feat(Email): send text email service

--- src/main/java/com/sontaypham/storemvc/service/EmailService.java
+++ src/main/java/com/sontaypham/storemvc/service/EmailService.java
@@ -1,6 +1,9 @@
 package com.sontaypham.storemvc.service;
 
+import com.sontaypham.storemvc.dto.response.email.EmailResponse;
+import com.sontaypham.storemvc.model.EmailDetails;
+
 public interface EmailService {
-    void sendTextEmail(Object object);
-    void sendTemplateEmail(Object object);
+    EmailResponse sendTextEmail(EmailDetails emailDetails);
+    EmailResponse sendTemplateEmail(EmailDetails emailDetails);
 }
--- src/main/java/com/sontaypham/storemvc/service/impl/EmailServiceImpl.java
+++ src/main/java/com/sontaypham/storemvc/service/impl/EmailServiceImpl.java
@@ -1,11 +1,14 @@
 package com.sontaypham.storemvc.service.impl;
 
+import com.sontaypham.storemvc.dto.response.email.EmailResponse;
+import com.sontaypham.storemvc.model.EmailDetails;
 import com.sontaypham.storemvc.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
@@ -15,18 +18,33 @@ import org.thymeleaf.TemplateEngine;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Slf4j
 public class EmailServiceImpl implements EmailService {
-    @Value("${SPRING_INIT_ADMIN_EMAIL}")
+    @Value("${SPRING_ADMIN_EMAIL_SERVICE}")
     String host;
     final JavaMailSender javaMailSender;
     final TemplateEngine templateEngine;
 
     @Override
-    public void sendTextEmail(Object object) {
-
+    public EmailResponse sendTextEmail(EmailDetails emailDetails) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(host);
+            message.setTo(emailDetails.getTo());
+            message.setSubject(emailDetails.getSubject());
+            message.setText(emailDetails.getMessageBody());
+            javaMailSender.send(message);
+            return EmailResponse.builder()
+                                .success(true)
+                                .message("Email sent successfully to " + emailDetails.getTo())
+                                .build();
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return EmailResponse.builder().message("Failed to sending email").success(false).build();
+        }
+        return null;
     }
 
     @Override
-    public void sendTemplateEmail(Object object) {
-
+    public EmailResponse sendTemplateEmail(EmailDetails emailDetails) {
+        return null;
     }
 }
