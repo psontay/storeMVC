966416d5562d16ff12e90a6dfabf7a3facdcde0b
sontaypham <phamsontay2206@gmail.com>
2026-01-01 09:49:31 +0700

    feat(User): delete feat

--- src/main/java/com/sontaypham/storemvc/repository/UserRepository.java
+++ src/main/java/com/sontaypham/storemvc/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.sontaypham.storemvc.repository;
 import com.sontaypham.storemvc.model.User;
 import java.util.Optional;
 import java.util.UUID;
+
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -32,4 +35,18 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     select u from User u where lower(u.username) like lower(concat('%' , :username , '%') ) or lower(u.email) like lower(concat( '%' , :email , '%'))
 """)
   Page<User> findByUsernameOrEmailContainingIgnoreCase(String username, String email, Pageable pageable);
+  // delete feat
+
+    @Query(value = "select * from users where deleted_at is not null", countQuery = "select count (*) from  users " +
+                                                                                    "where deleted_at is not null", nativeQuery = true)
+    Page<User> findAllDeleted(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update users set deleted_at = null where id = :id" , nativeQuery = true)
+    void restore(UUID id);
+    @Transactional
+    @Modifying
+    @Query(value = "delete from users where id = :id", nativeQuery = true)
+    void hardDelete(UUID id);
 }
